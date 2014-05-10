8d3bc8ad4891510a5cbc85bcc5ebc36c7e8db2bd
Dan Savilonis <djs@n-cube.org>
2014-05-10 05:41:14 -0400

    Move module require inside prompt filter
    This is required otherwise clink will only work to print out the prompt
    on the first attempt. Subsequent attempts will see git_prompt set to nil.

--- git_prompt.lua
+++ git_prompt.lua
@@ -1,6 +1,27 @@
-local git_prompt = require('git_prompt_lib')
+--
+-- Copyright (c) 2013-2014 Dan Savilonis
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+--
 
 function git_prompt_filter()
+    local git_prompt = require('git_prompt_lib')
     ps1 = git_prompt.git_ps1()
     if ps1 ~= "" then
         git_prompt = clink.prompt.value .. " (" .. ps1 .. ") "
@@ -10,4 +31,4 @@ function git_prompt_filter()
     return false
 end
 
-clink.prompt.register_filter(git_prompt_filter, 50)
+clink.prompt.register_filter(git_prompt_filter, 49)
